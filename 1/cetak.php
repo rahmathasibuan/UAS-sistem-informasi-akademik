1d12522068c36eb185bc5de89723301bb561cc85
Dollar Rahmat Hasibuan <81568130+rahmathasibuan@users.noreply.github.com>
2021-07-18 01:44:32 +0700

    Add files via upload

---
+++ 1/cetak.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>cetak </title>
+</head>
+<body>
+ 
+<center>
+		
+		
+	</center>
+ 
+	<br/>
+ 
+	<p>
+		rahmat hasiban
+	</p>
+ 
+	<p>
+		311910026.
+	</p>
+ 
+	<script>
+		window.print();
+	</script>
+	
+</body>
+</html>
---
+++ 1/content_footer.php
@@ -0,0 +1,4 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs"></div>
+    <strong>Rahmat Hasibuan &copy; <?php echo date("Y") ?>
+</footer>
