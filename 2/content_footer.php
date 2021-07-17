fd0eb260d613308336cd17ce1d2270660c35c842
Dollar Rahmat Hasibuan <81568130+rahmathasibuan@users.noreply.github.com>
2021-07-18 01:46:29 +0700

    Add files via upload

---
+++ 2/content_footer.php
@@ -0,0 +1,4 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs"></div>
+    <strong>Copyright &copy; <?php echo date("Y") ?> uas basis data Rahmat Hasibuan</a></strong>
+</footer>
